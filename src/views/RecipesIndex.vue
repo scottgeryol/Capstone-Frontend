f6d3413308c8ba79168d5d420206caaf6893e552
Scott Geryol <sgeryol@gmail.com>
2020-05-07 21:01:18 -0500

    Initial commit

---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>All Recipes</h1>
+    <div v-for="recipe in recipes">
+      <h2>{{ recipe.name }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      recipes: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      this.recipes = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
